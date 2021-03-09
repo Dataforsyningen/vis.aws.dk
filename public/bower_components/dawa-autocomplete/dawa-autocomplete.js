e80b47e60f71d97da765e5d59c1f1d5909323695
Signe <signees96@gmail.com>
2021-03-09 13:49:59 +0100

    Fixed subdomain and domain in dawa-autocomplete.js

--- public/bower_components/dawa-autocomplete/dawa-autocomplete.js
+++ public/bower_components/dawa-autocomplete/dawa-autocomplete.js
@@ -1,7 +1,7 @@
 $.widget("dawa.dawaautocomplete", {
 	options: {
 		jsonp: !("withCredentials" in (new XMLHttpRequest())),
-		baseUrl: 'https://dawa.aws.dk',
+		baseUrl: 'https://api.dataforsyningen.dk',
 		minLength: 2,
 		delay: 0,
 		adgangsadresserOnly: false,
